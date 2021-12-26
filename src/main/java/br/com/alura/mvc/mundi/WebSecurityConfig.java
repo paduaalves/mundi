e13b285a55421fbf169f9feed00cfae5cef30fc0
Pádua Alves <paduaalves03@gmail.com>
2021-12-26 16:23:37 -0300

    Recuperando usuário de login através do BD

--- src/main/java/br/com/alura/mvc/mundi/WebSecurityConfig.java
+++ src/main/java/br/com/alura/mvc/mundi/WebSecurityConfig.java
@@ -1,36 +1,38 @@
 package br.com.alura.mvc.mundi;
 
-import org.springframework.context.annotation.Bean;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest()
-        .authenticated()
-        .and()
-        .formLogin(form -> form
-        .loginPage("/login")
-        .permitAll())
-        .logout(logout->logout.logoutUrl("/logout"));
+                .authenticated()
+                .and()
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll().defaultSuccessUrl("/home", true))
+                .logout(logout -> logout.logoutUrl("/logout"));
     }
 
     @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        // TODO Auto-generated method stub
-        UserDetails user = User.withDefaultPasswordEncoder().username("joao")
-                .password("joao").roles("ADM").build();
-        return new InMemoryUserDetailsManager(user);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder);
+
     }
+
 }
- 
